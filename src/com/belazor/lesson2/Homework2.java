86b852856a9ea60289fa69765465f98492ee1680
Tatsiana_Belazor <Tatsiana_Belazor@epam.com>
2018-01-03 15:35:51 +0300

    commit homework with loop

--- src/com/belazor/lesson2/Homework2.java
+++ src/com/belazor/lesson2/Homework2.java
@@ -12,13 +12,13 @@ public class Homework2 {
 
     public static void main(String[] args) {
 
-        Award myAward1 = new Award(25);
-        Award myAward2 = new Award("EUR", 10);
-        Award myAward3 = new Award("EUR", 15);
-        Award awardWithSoli = new Award (20, 1000);
+        Award myAward1 = new Award(10);
+        Award myAward2 = new Award("EUR", 25);
+        Award myAward3 = new Award("EUR", 35);
+        Award awardWithSoli = new Award(20, 1000);
         Nominee myNominee = new Nominee("Boss");
         Nominee myNominee1 = new Nominee("John");
-        Nominator myNominator1 = new Nominator("Adam",3, 200);
+        Nominator myNominator1 = new Nominator("Adam", 3, 200);
         Nominee myNominee2 = new Nominee("Jack", 2, 100);
 
         System.out.println(String.format("Recipient of the award is %s", myNominee.getName()));
@@ -40,12 +40,39 @@ public class Homework2 {
 
         myNominator.nominate(nomineesName, myAward1);
 
+        myNominee.calculation(myAward1);
+        myNominee1.calculation(myAward1);
+        myNominee2.calculation(myAward1);
 
-        myNominator1.nominateTillNomineeAwardQuantityLimit(myNominee2, myAward2);
+
+        double nominee1 = myNominee.calculation(myAward1);
+        System.out.println(String.format("First quantity is %s", nominee1));
+
+        double nominee2 = myNominee1.calculation(myAward2);
+        System.out.println(String.format("Second quantity is %s", nominee2));
+
+        double nominee3 = myNominee2.calculation(myAward3);
+        System.out.println(String.format("Third quantity is %s", nominee3));
+
+
+        if (nominee1 < nominee2) {
+            System.out.println(String.format("%s award is smaller than %s award", myNominee.getName(), myNominee1.getName()));
+        }
+        if (nominee1 >= nominee2) {
+            System.out.println(String.format("%s award is bigger or equal to %s award", myNominee.getName(), myNominee1.getName()));
+        }
+        if (nominee1 != nominee2) {
+            System.out.println(String.format("%s award is not equal to %s award", myNominee.getName(), myNominee1.getName()));
+        }
+        if (nominee1 < nominee2 && nominee1 < nominee3) {
+            System.out.println(String.format("%s award is the smallest", myNominee.getName()));
+        }
+
+
         myNominator1.nominateTillNominatorAwardQuantityLimit(myNominee2, myAward2);
         myNominator1.nominateTillNominatorAwardAmountLimit(myNominee2, myAward2);
+        myNominator1.nominateTillNomineeAwardQuantityLimit(myNominee2, myAward2);
         myNominator1.nominateTillNomineeAwardAmountLimit(myNominee2, myAward2);
-
 
 
     }
--- src/com/belazor/lesson2/nominator/Nominator.java
+++ src/com/belazor/lesson2/nominator/Nominator.java
@@ -54,7 +54,42 @@ public class Nominator {
 
     }
 
-    //public void maxNominatorQuantity (Nominee nominee, Award award) {
+    /*
+    3.1 Nominator дает авадру Nominee пока не достигнет nominatorAwardQuantityLimit
+     */
+
+    public void nominateTillNominatorAwardQuantityLimit(Nominee nominee, Award award) {
+        int nominationsCount = 1;
+        int count = 0;
+        do {
+            nominee.receiveAward(award);
+            count++;
+            System.out.println(String.format("%s gives %s EUR to %s. Without restrictions", name, award.getValue(), nominee.getName()));
+            nominationsCount++;
+        } while (nominationsCount <= getNominatorAwardQuantityLimit());
+
+        System.out.println(String.format("Total number of given awards is %s", count));
+    }
+
+    /*
+    3.2 Nominator дает авадру Nominee пока не достигнет nominatorAwardAmountLimit
+     */
+
+    public void nominateTillNominatorAwardAmountLimit(Nominee nominee, Award award) {
+        int totalAwardAmountForNominator = 0;
+        int count = 0;
+        while (totalAwardAmountForNominator + award.getValue() <= getNominatorAwardAmountLimit()) {
+            totalAwardAmountForNominator += award.getValue();
+            nominee.receiveAward(award);
+            count++;
+            System.out.println(String.format("%s gives %s EUR to %s. Without restrictions", name, award.getValue(), nominee.getName()));
+        }
+        System.out.println(String.format("Total number of given awards is %s", count));
+    }
+
+    /*
+    3.3 Nominator дает авадру Nominee пока не достигнет nomineeAwardQuantityLimit
+     */
 
 
     public void nominateTillNomineeAwardQuantityLimit(Nominee nominee, Award award) {
@@ -76,32 +111,9 @@ public class Nominator {
         }
     }
 
-
-    public void nominateTillNominatorAwardQuantityLimit(Nominee nominee, Award award) {
-        int nominationsCount = 1;
-        int count = 0;
-        do {
-            nominee.receiveAward(award);
-            count++;
-            System.out.println(String.format("%s gives %s EUR to %s. Without restrictions", name, award.getValue(), nominee.getName()));
-            nominationsCount++;
-        } while (nominationsCount <= getNominatorAwardQuantityLimit());
-
-        System.out.println(String.format("Total number of given awards is %s", count));
-    }
-
-
-    public void nominateTillNominatorAwardAmountLimit(Nominee nominee, Award award) {
-        int totalAwardAmountForNominator = 0;
-        int count = 0;
-        while (totalAwardAmountForNominator + award.getValue() <= getNominatorAwardAmountLimit()) {
-            totalAwardAmountForNominator += award.getValue();
-            nominee.receiveAward(award);
-            count++;
-            System.out.println(String.format("%s gives %s EUR to %s. Without restrictions", name, award.getValue(), nominee.getName()));
-        }
-        System.out.println(String.format("Total number of given awards is %s", count));
-    }
+    /*
+    3.4 Nominator дает авадру Nominee пока не достигнет nomineeAwardAmountLimit
+     */
 
     public void nominateTillNomineeAwardAmountLimit(Nominee nominee, Award award) {
         int totalAwardAmountForNominee = 0;
